9f0823c088dd3e2ce9fb736416e956471e9fbcdc
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2025-01-20 22:20:01 +0530

    4th

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -626,6 +626,7 @@ export default function ProductDetail() {
             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           },
         ],
+        avgRating: 4.8,
       },
 
     ]
