e4fcbb00aa97cea65d10752d3b25274dbe0a7176
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2025-01-20 22:35:02 +0530

    18th

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -13,25 +13,21 @@ import productImg07 from "../Images/arm-chair-01.jpg";
 import productImg08 from "../Images/arm-chair-02.jpg";
 import productImg09 from "../Images/arm-chair-03.jpg";
 import productImg10 from "../Images/arm-chair-01.jpg";
-
 import phone01 from "../Images/phone-01.jpg";
 import phone02 from "../Images/phone-02.jpg";
 import phone03 from "../Images/phone-03.png";
 import phone04 from "../Images/phone-04.jpg";
 import phone05 from "../Images/phone-05.jpg";
 import phone06 from "../Images/phone-06.jpg";
-
 import watch01 from "../Images/watch-01.jpg";
 import watch02 from "../Images/watch-02.jpg";
 import watch03 from "../Images/watch-03.jpg";
 import watch04 from "../Images/watch-04.jpg";
-
 import wireless01 from "../Images/wireless-01.png";
 import wireless02 from "../Images/wireless-02.png";
 import wireless03 from "../Images/wireless-03.png";
 import wireless04 from "../Images/wireless-04.png";
 import table from '../Images/table.jpg';
-
 export default function ProductDetail() {
   const { products, setProducts, addtocart, qty, handleQty } = useContext(AppContext);
   const location = useLocation();
@@ -628,7 +624,6 @@ export default function ProductDetail() {
         ],
         avgRating: 4.8,
       },
-
     ]
     )
   }, [setProducts])
